04ce2b7807b34a44a99e472e47324e7e18cc1dba
Maxim <murmax82@gmail.com>
2025-03-24 19:49:19 +0400

    added models

--- composeApp/src/commonMain/kotlin/org/bibletranslationtools/wat/domain/Settings.kt
+++ composeApp/src/commonMain/kotlin/org/bibletranslationtools/wat/domain/Settings.kt
@@ -16,9 +16,25 @@ enum class Theme {
 
 enum class Model(val value: String) {
     GPT_4_O("gpt-4o"),
+    GPT_4_TURBO("gpt-4-turbo"),
+    GPT_3_5_TURBO("gpt-3.5-turbo"),
+    O_3_MINI("o3-mini"),
+    O_1("o1"),
+    O_1_MINI("o3-mini"),
+    CLAUDE_3_7_SONNET_LATEST("claude-3-7-sonnet-latest"),
+    CLAUDE_3_5_SONNET_LATEST("claude-3-5-sonnet-latest"),
     CLAUDE_3_5_HAIKU_LATEST("claude-3-5-haiku-latest"),
+    CLAUDE_3_OPUS_LATEST("claude-3-opus-latest"),
     MINISTRAL_3B_LATEST("ministral-3b-latest"),
-    QWEN_2_5_7B_INSTRUCT("qwen2.5-7b-instruct")
+    CODESTRAL_LATEST("codestral-latest"),
+    MINISTRAL_LARGE_LATEST("mistral-large-latest"),
+    PIXTRAL_LARGE_LATEST("pixtral-large-latest"),
+    MINISTRAL_8B_LATEST("ministral-8b-latest"),
+    QWEN_2_5_7B_INSTRUCT("qwen2.5-7b-instruct"),
+    QWEN_2_5_14B_INSTRUCT("qwen2.5-14b-instruct"),
+    QWEN_MAX("qwen-max"),
+    QWEN_PLUS("qwen-plus"),
+    QWEN_TURBO("qwen-turbo"),
 }
 
 enum class Locales(val value: String) {
